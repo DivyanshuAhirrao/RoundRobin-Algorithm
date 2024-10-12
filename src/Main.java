ba8460c1b69845b37647d0bf84e1361ca74d0b17
Divyanshu Ahirrao <246divyanahuahirrao@gmail.com>
2024-10-12 23:06:40 +0530

    Initial commit for Round Robin Algorithm

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
         RoundRobinScheduler scheduler = new RoundRobinScheduler(3);
         scheduler.addProcess(new Process("Server 1", 10));
+        System.out.println("111");
         scheduler.addProcess(new Process("Server 2", 5));
+        System.out.println("222");
+
         scheduler.addProcess(new Process("Server 3", 8));
+        System.out.println("333");
+
     }
 }
